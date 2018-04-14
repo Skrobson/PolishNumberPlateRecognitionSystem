6babe654a658315a3dede06383eec1c6e4236ab0
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2018-04-14 20:58:11 +0200

    first version of detecting plates (Grzegorz & Patryk)

---
+++ PolishPlateRecognition/PlateSearch.h
@@ -0,0 +1,37 @@
+
+#include "stdafx.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <ctime>
+#include <iostream>
+#include "Plate.h"
+#include <ctime>
+
+using namespace std;
+
+class PlateSearch
+{
+public:
+	PlateSearch();
+	PlateSearch(String path);
+	~PlateSearch();
+
+	void convert();
+	vector<Mat> getMatVector();
+	
+	
+
+private:
+	Mat correctGamma(Mat& img, double gamma);
+	void findPlate();
+	void cut(Mat input);
+	bool verifySizes(RotatedRect candidate);
+	Mat img_gray;
+	Mat image;
+	vector<Plate> output;
+	vector<Mat> number;
+};
+
+
+
